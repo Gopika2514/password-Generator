0a570d4d4bcdbc23e65d254ce8a0f940e7cfb4d2
gopika <gopipatel2514@gmail.com>
2021-06-07 19:46:05 -0700

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 // Assignment Code
 // Declaring variables containing arrays to choose and pull from for passGen
-let specialChar = ["!@#$%^&*"];
-let numbArray = ["0123456789"];
-let lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
-let upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+let specialChar = "!@#$%^&*";
+let numbArray = "0123456789";
+let lowerCase = "abcdefghijklmnopqrstuvwxyz";
+let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 //Create arrays from strings of characters
 //No need for the below extra step. The split method can apply directly to the end of the string.
@@ -88,7 +88,6 @@ function userPrompt() {
 function getRandomElement(arr) {
   var random = Math.floor(Math.random() * arr.length);
   var rand = arr[random];
-
   return rand;
   //iterate once per length specified by user
 }
@@ -99,29 +98,37 @@ function generatingPassword() {
   // stores final password
   var passResult = [];
   // stores the possible characters to be included in password
-  var possibleChars = [];
+  var possibleChars = "";
   // create a variable to store guaranteed characters
   var guarChar = [];
 
   if (options.specialChar) {
     possibleChars = possibleChars.concat(specialChar);
+    passResult.push(getRandomElement(specialChar));
+    console.log(passResult[passResult.length-1]);
   }
   if (options.lowerCase) {
     possibleChars = possibleChars.concat(lowerCase);
+    passResult.push(getRandomElement(lowerCase));
+    console.log(passResult[passResult.length-1]);
   }
   if (options.upperCase) {
     possibleChars = possibleChars.concat(upperCase);
+    passResult.push(getRandomElement(upperCase));
+    console.log(passResult[passResult.length-1]);
   }
   if (options.numbArray) {
     possibleChars = possibleChars.concat(numbArray);
+    passResult.push(getRandomElement(numbArray));
+    console.log(passResult[passResult.length-1]);
   }
-
-  for (let i = 0; i < options.length; i++) {
+  console.log(passResult.length);
+  for (let i = passResult.length; i < options.length; i++) {
     passResult[i] = getRandomElement(possibleChars);
   }
   console.log(passResult);
-  passResult = passResult.join("");
-  return passResult;
+  // passResult = passResult.join("");
+  return passResult.join("");
 }
 
 var generateBtn = document.querySelector("#generate");
